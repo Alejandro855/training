a9b32a4133d73dd2fb3388a654be2225895fc2b3
alejandro.chaparro <alejandro855@gmail.com>
2015-08-05 08:13:38 -0500

    Archetype added to main message.

--- MyFirstProject/src/main/java/Main.java
+++ MyFirstProject/src/main/java/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out
-				.println("********* THIS IS MY FIRST MAVEN PROGRAM **********");
+				.println("********* THIS IS MY FIRST QUICKSTART MAVEN PROGRAM **********");
 		System.out.println("Helooooo worllllldddd!!!!!");
 
 	}
